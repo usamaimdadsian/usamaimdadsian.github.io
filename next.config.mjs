5f0acca0f7450a76460bc432263e4249653c1f04
Usama Imdad <usamaimdadsian@gmail.com>
2024-12-27 23:12:54 +0500

    setup for github

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/usamaimdadsian.github.io/' : '',
+  basePath: isProd ? '/usamaimdadsian.github.io' : '',
+  output: 'export'
+};
+
 
 export default nextConfig;
