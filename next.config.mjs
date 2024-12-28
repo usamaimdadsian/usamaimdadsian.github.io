47aa67995a74fbd8eab9257a394267c57a879c11
Usama Imdad <usamaimdadsian@gmail.com>
2024-12-28 09:45:38 +0500

    setup for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
@@ -10,6 +9,4 @@ const nextConfig = {
   basePath: isProd ? '/usamaimdadsian.github.io' : '',
   output: 'export'
 };
-
-
 export default nextConfig;
